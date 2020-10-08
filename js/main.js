fb5ee21a91f4db0f4465d0ea372dd4b5209e58e3
maadcode <manueldavila.developer@gmail.com>
2020-10-07 22:37:47 -0500

    Adding javascript for date and mopup

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 const $btnNotify = document.querySelector('.notify')
 const $btnClose = document.querySelector('.close')
 
+const $day = document.getElementById('day')
+const $hour = document.getElementById('hour')
+const $minute = document.getElementById('minute')
+const $second = document.getElementById('second')
+
+setInterval(() => {
+    let day = new Date().getDay()
+    let hour = new Date().getHours()
+    let minute = new Date().getMinutes()
+    let second = new Date().getSeconds()
+
+    $day.innerText = day
+    $hour.innerText = hour
+    $minute.innerText = minute
+    $second.innerText = second
+}, 1000)
+
+$btnNotify.addEventListener('click', toggle)
+$btnClose.addEventListener('click', toggle)
+
 function toggle(ev) {
     ev.preventDefault()
     const $newsletter = document.querySelector('.newsletter')
     $newsletter.classList.toggle('active')
 }
-
-$btnNotify.addEventListener('click', toggle)
-$btnClose.addEventListener('click', toggle)
